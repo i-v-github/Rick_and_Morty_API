a218ecf17d2212c7dc2c7702d017f1ef058196cb
Ilya <ilya.vedernikov+github@gmail.com>
2022-12-08 20:35:21 +0100

    updated code

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 import requests
 
+base_url = 'https://rickandmortyapi.com/api'
+
+
+def main_request(endpoint):
+    response = requests.get(base_url + endpoint)
+    return response.json()
+
+
+def get_pages(response):
+    return response.get('info').get('pages')
+
+def parse_json(response):
+    for character in response.get('results'):
+        print(character.get('name'),len(character.get('episode')))
+    return
+
+
+
 if __name__ == '__main__':
-    base_url = 'https://rickandmortyapi.com/api'
     endpoint = '/character'
 
-    response = requests.get(base_url + endpoint)
+    data = main_request(endpoint)
 
-    print(response.text)
+    pages = get_pages(data)
 
-    data = response.json()
+    print(pages)
 
-    print(data)
+    parse_json(data)
